de5d5d42c0effeb2df7beb833966e02121984d46
Jakub Schulz <48328739+szulcus@users.noreply.github.com>
2024-03-08 03:38:18 +0100

    Update Intro.vue

--- components/home/Intro.vue
+++ components/home/Intro.vue
@@ -8,7 +8,7 @@
 		<p class="intro__content">
 			Z miłością, pasją i sercem szydełkuję eleganckie, stylowe ubranka do świeczek. Wykonuję je z bawełny merceryzowanej. Można ich używać wielokrotnie. Do każdego stworzonego przeze mnie produktu dołączam świeczkę wykonaną z wosku roślinnego.
 			<br /><br />
-			Moja oferta obejmuje cztery rozmiary - S i M są bezzapachowe (przy większym zamówieniu, za dodatkową opłatą 3zł/szt. istnieje możliwość wyboru świeczki sojowej, zapachowej), L - o kojącym aromacie lasu i bursztynu z pikantnymi nutami cytrusów, natomiast XL to bezzapachowe świeczki bryłowe.
+			Moja oferta obejmuje cztery rozmiary - S i M są bezzapachowe (przy większym zamówieniu, za dodatkową opłatą 5zł/szt. istnieje możliwość wyboru świeczki sojowej, zapachowej), L - o kojącym aromacie lasu i bursztynu z pikantnymi nutami cytrusów, natomiast XL to bezzapachowe świeczki bryłowe.
 		</p>
 	</div>
 </template>
